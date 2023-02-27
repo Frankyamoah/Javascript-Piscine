d5d988208e0a028420566744b844196e4e0469cd
fyamoah <frankyamoah94@gmail.com>
2023-02-27 16:05:32 +0000

    new

---
+++ last-first-kiss.js
@@ -0,0 +1,14 @@
+function first(arr){
+return arr[0]
+}
+
+function last(arr){
+return arr.length - 1
+
+}
+
+function kiss(arr){
+let first = arr[0]
+let last = arr.length - 1
+return first, last
+}
