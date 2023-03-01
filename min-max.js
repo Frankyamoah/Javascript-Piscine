45b3e1b99b2d25e0cb6c675cfd5738bcf6cacf60
fyamoah <frankyamoah94@gmail.com>
2023-03-01 22:51:40 +0000

    hmm

---
+++ min-max.js
@@ -0,0 +1,18 @@
+// Function to find maximum of two numbers
+function max(num1, num2) {
+    if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
+  
+  // Function to find minimum of two numbers
+  function min(num1, num2) {
+    if (num1 < num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
+  
