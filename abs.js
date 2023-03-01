13872b3e2e23020a58dae58b6eac4b0ca57608f2
fyamoah <frankyamoah94@gmail.com>
2023-03-01 22:49:37 +0000

    hmm

---
+++ abs.js
@@ -0,0 +1,14 @@
+// Function to check if a number is positive
+function isPositive(num) {
+    return num > 0;
+  }
+  
+  // Function to calculate absolute value of a number
+  function abs(num) {
+    if (num >= 0) {
+      return num;
+    } else {
+      return -num;
+    }
+  }
+  
