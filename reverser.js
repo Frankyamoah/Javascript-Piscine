ecb6ace7fd8f3491ed716a08343a65e2c2eca6dc
fyamoah <frankyamoah94@gmail.com>
2023-03-06 12:21:28 +0000

    be great

--- reverser.js
+++ reverser.js
@@ -3,7 +3,7 @@ function reverse(str){
     for (let i = str.length - 1; i >= 0; i--){
        result.push(str[i]);
        if (str === 'string'){
-        String.join(result)
+        result.join ("")
        }
    }
     return result;
