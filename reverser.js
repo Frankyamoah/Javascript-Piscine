ffa05b268df3fbdae677c9a38764f746da8e11d5
fyamoah <frankyamoah94@gmail.com>
2023-03-06 00:08:40 +0000

    be great

--- reverser.js
+++ reverser.js
@@ -1,4 +1,7 @@
 function reverse(str){
+    if (str === 'string') {
+        str = str.split('')
+    }
     result = [];
     for (let i = str.length - 1; i >= 0; i--){
        result.push(str[i]);
@@ -6,5 +9,6 @@ function reverse(str){
     return result;
 }
 
+console.log(reverse("[1, 2, 3]), [3, 2, 1]"))
 
 
