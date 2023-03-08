48031cfc67ca7ec43e3ff5f145ad0d2ef428ccc4
fyamoah <frankyamoah94@gmail.com>
2023-03-08 17:20:36 +0000

    help

--- get-some-time.js
+++ get-some-time.js
@@ -1,18 +1,26 @@
 function firstDayWeek(week, year) {
-    const date = new Date(`${year}-01-01`);
-    const day = date.getDay() || 7;
+    const date = new Date(year);
+    const day = date.getDay();
     
-    if (day !== 1) {
-      date.setHours(-24 * (day - 1));
-    }
+    // Calculate the date for the first day of the year
+    const firstDayOfYear = new Date(year);
+    firstDayOfYear.setDate(1);
+    firstDayOfYear.setMonth(0);
     
-    date.setDate(date.getDate() + (week - 1) * 7);
-    
-    const yearOfWeek = date.getFullYear();
-    const monthOfWeek = date.getMonth() + 1;
-    const dayOfWeek = date.getDate();
-    
-    return `${dayOfWeek < 10 ? '0' : ''}${dayOfWeek}-${monthOfWeek < 10 ? '0' : ''}${monthOfWeek}-${yearOfWeek}`;
-  }
+    // Adjust the date for the first day of the first week
+    const daysToMonday = 1 - day;
+    const firstDayOfFirstWeek = new Date(firstDayOfYear);
+    firstDayOfFirstWeek.setDate(firstDayOfYear.getDate() + daysToMonday);
   
+    // Calculate the date for the first day of the given week
+    const daysToFirstDayOfWeek = (week - 1) * 7;
+    const firstDayOfWeek = new Date(firstDayOfFirstWeek);
+    firstDayOfWeek.setDate(firstDayOfFirstWeek.getDate() + daysToFirstDayOfWeek);
+  
+    const dd = String(firstDayOfWeek.getDate()).padStart(2, '0');
+    const mm = String(firstDayOfWeek.getMonth() + 1).padStart(2, '0');
+    const yyyy = String(firstDayOfWeek.getFullYear());
+    
+    return `${dd}-${mm}-${yyyy}`;
+  }
   
