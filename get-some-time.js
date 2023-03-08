4da82cafbef9184e79d8f1c777028274d7f823c1
fyamoah <frankyamoah94@gmail.com>
2023-03-08 23:52:31 +0000

    help

--- get-some-time.js
+++ get-some-time.js
@@ -1,26 +1,18 @@
 function firstDayWeek(week, year) {
-    const date = new Date(year);
-    const day = date.getDay();
-    
-    // Calculate the date for the first day of the year
-    const firstDayOfYear = new Date(year);
-    firstDayOfYear.setDate(1);
-    firstDayOfYear.setMonth(0);
-    
-    // Adjust the date for the first day of the first week
-    const daysToMonday = 1 - day;
-    const firstDayOfFirstWeek = new Date(firstDayOfYear);
-    firstDayOfFirstWeek.setDate(firstDayOfYear.getDate() + daysToMonday);
-  
-    // Calculate the date for the first day of the given week
-    const daysToFirstDayOfWeek = (week - 1) * 7;
-    const firstDayOfWeek = new Date(firstDayOfFirstWeek);
-    firstDayOfWeek.setDate(firstDayOfFirstWeek.getDate() + daysToFirstDayOfWeek);
-  
-    const dd = String(firstDayOfWeek.getDate()).padStart(2, '0');
-    const mm = String(firstDayOfWeek.getMonth() + 1).padStart(2, '0');
-    const yyyy = String(firstDayOfWeek.getFullYear());
-    
-    return `${dd}-${mm}-${yyyy}`;
-  }
-  
+    let dd, mm, yyyy
+    const year2 = new Date(year)
+    const oneDay = 24 * 60 * 60 * 1000
+    if (week === 1) {
+        dd = year2.getDate().toString().padStart(2, '0')
+        mm = (year2.getMonth()+1).toString().padStart(2, '0')
+        yyyy = year2.getFullYear().toString().padStart(4, '0')
+        return `${dd}-${mm}-${yyyy}`
+    }
+    const dateOfWeek = new Date((week - 1) * oneDay * 7 + year2.getTime())
+    const monday = dateOfWeek.getDay() == 0? 7:dateOfWeek.getDay()
+    const firstDate = new Date(dateOfWeek.getTime() - ((monday - 1) * oneDay))
+    dd = firstDate.getDate().toString().padStart(2, '0')
+    mm = (firstDate.getMonth()+1).toString().padStart(2, '0')
+    yyyy = firstDate.getFullYear().toString().padStart(4, '0')
+    return `${dd}-${mm}-${yyyy}`
+}
