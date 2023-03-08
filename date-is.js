8b16af1db03ebe2e0b9b177aa1b47864050d54e6
fyamoah <frankyamoah94@gmail.com>
2023-03-08 15:13:04 +0000

    doctor Strange

--- date-is.js
+++ date-is.js
@@ -4,10 +4,17 @@ function isValid(date) {
   }
   
   
-  // isAfter: accepts two Date arguments, and returns true if the first is greater then the second.
-  function isAfter(date1, date2) {
+// isAfter: accepts two Date arguments, and returns true if the first is greater then the second.
+function isAfter(date1, date2) {
+    if (!(date1 instanceof Date) || isNaN(date1.getTime())) {
+      return false; // date1 is invalid
+    }
+    if (!(date2 instanceof Date) || isNaN(date2.getTime())) {
+      return false; // date2 is invalid
+    }
     return date1.getTime() > date2.getTime(); // returns true if date1 is after date2, false otherwise
   }
+  
   
   // isBefore: accepts two Date arguments, and returns true if the second is greater than the first.
   function isBefore(date1, date2) {
