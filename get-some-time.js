d81dc902f526a5a40ccc961555ddd0a1e376f520
fyamoah <frankyamoah94@gmail.com>
2023-03-08 17:10:20 +0000

    help

--- get-some-time.js
+++ get-some-time.js
@@ -1,28 +1,14 @@
-function firstDayWeek(week, year) {
-    const firstDayOfYear = new Date(year, 0, 1); // January 1st of the year
-    const firstDay = new Date(firstDayOfYear.getTime()); // create a copy of the first day of the year
-  
-    // find the first Monday of the year
-    while (firstDay.getDay() !== 1) {
-      firstDay.setDate(firstDay.getDate() + 1);
-    }
-  
-    // calculate the number of days to add to the first Monday to get to the first day of the given week
-    const daysToAdd = (week - 1) * 7;
-  
-    // add the days to the first Monday to get to the first day of the given week
-    firstDay.setDate(firstDay.getDate() + daysToAdd);
-  
-    // if the resulting date is in a previous year, return the first day of the year instead
-    if (firstDay.getFullYear() < year) {
-      return `01-01-${year}`;
-    }
-  
-    // format the resulting date to the desired format
-    const day = firstDay.getDate().toString().padStart(2, '0');
-    const month = (firstDay.getMonth() + 1).toString().padStart(2, '0');
-    const formattedYear = firstDay.getFullYear().toString();
-  
-    return `${day}-${month}-${formattedYear}`;
+function firstDayWeek(weekNumber, year) {
+    const date = new Date(year);
+    const dayOfWeek = date.getDay();
+    const diff = 1 - dayOfWeek;
+    const januaryFirst = new Date(year, 0, 1);
+    const firstMonday = new Date(januaryFirst.setDate(januaryFirst.getDate() + diff));
+    const daysToAdd = (weekNumber - 1) * 7;
+    const result = new Date(firstMonday.setDate(firstMonday.getDate() + daysToAdd));
+    const dd = String(result.getDate()).padStart(2, '0');
+    const mm = String(result.getMonth() + 1).padStart(2, '0');
+    const yyyy = result.getFullYear();
+    return `${dd}-${mm}-${yyyy}`;
   }
   
