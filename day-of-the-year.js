64353540a02def15b5f6473d3f56be1bbfcb2601
fyamoah <frankyamoah94@gmail.com>
2023-03-09 00:04:10 +0000

    help

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,14 +1,13 @@
+/**
+ * Returns the number of days since the first day of the year.
+ * @param {Date} date - The input date.
+ * @returns {number} The number of days since the first day of the year.
+ */
 function dayOfTheYear(date) {
-    // Create a new date object with the same year as the input date, but with a month and day of 1
     const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  
-    // Calculate the difference in milliseconds between the input date and the first day of the year
-    const timeDiff = date.getTime() - firstDayOfYear.getTime();
-  
-    // Calculate the number of days between the two dates by dividing the time difference by the number of milliseconds in a day
-    const dayOfYear = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  
-    // Return the day of the year
-    return dayOfYear + 1; // Add 1 to account for the first day of the year being day 1
+    const oneDay = 1000 * 60 * 60 * 24; // milliseconds in a day
+    const diff = date.getTime() - firstDayOfYear.getTime(); // difference in milliseconds
+    return Math.ceil(diff / oneDay) + 1; // add 1 to include the first day of the year
   }
+  
   
