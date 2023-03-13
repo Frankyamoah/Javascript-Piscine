72f6e4ff630f705602ced7f7cb8bf15f46f2a991
fyamoah <frankyamoah94@gmail.com>
2023-03-13 12:15:59 +0000

    woah

--- using-map.js
+++ using-map.js
@@ -27,20 +27,38 @@ function fahrenheitToCelsius(tempsFahrenheit) {
       temperature: obj.temperature.replace(/\s+/g, '')
     }));
   }
-  
-  function tempForecasts(data) {
-    return data.map((item) => {
-      // Extract values from the object
-      const { city, temperature, state } = item;
-  
-      // Convert temperature from Fahrenheit to Celsius and round down
-      const celsius = Math.floor((parseInt(temperature) - 32) * (5 / 9));
-  
-      // Capitalize the first letter of the state name
-      const capitalizedState = state.charAt(0).toUpperCase() + state.slice(1).toLowerCase();
-  
-      // Return formatted string using template literals
-      return `${celsius}°Celsius in ${city}, ${capitalizedState}`;
-    });
+
+
+
+  function tempForecasts(forecasts) {
+    const formattedForecasts = [];
+    for (let i = 0; i < forecasts.length; i++) {
+      const { city, temperature, state } = forecasts[i];
+      const celsius = ((parseFloat(temperature) - 32) * 5) / 9;
+      const formattedTemp = `${Math.floor(celsius)}°Celsius`;
+      const formattedState = state
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+      const formattedForecast = `${formattedTemp} in ${city}, ${formattedState}`;
+      formattedForecasts.push(formattedForecast);
+    }
+
+    return formattedForecasts;
   }
+//   function tempForecasts(data) {
+//     return data.map((item) => {
+//       // Extract values from the object
+//       const { city, temperature, state } = item;
+  
+//       // Convert temperature from Fahrenheit to Celsius and round down
+//       const celsius = Math.floor((parseInt(temperature) - 32) * (5 / 9));
+  
+//       // Capitalize the first letter of the state name
+//       const capitalizedState = state.charAt(0).toUpperCase() + state.slice(1).toLowerCase();
+  
+//       // Return formatted string using template literals
+//       return `${celsius}°Celsius in ${city}, ${capitalizedState}`;
+//     });
+//   }
   
