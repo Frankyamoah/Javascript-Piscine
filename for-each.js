4611be321f4b58e1a5108b2f45e1e74e2b8ce449
fyamoah <frankyamoah94@gmail.com>
2023-03-13 12:32:31 +0000

    woah

---
+++ for-each.js
@@ -0,0 +1,8 @@
+function forEach(arr, callback) {
+    // Use the map method to iterate over the array
+    arr.map((elem, index, array) => {
+      // Call the callback function on each element
+      callback(elem, index, array);
+    });
+  }
+  
